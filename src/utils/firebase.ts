83177230946975d1b302dcdc27205f9448ad2029
Gaurav Gupta <gauravgupta1533@gmail.com>
2021-06-06 12:14:36 +0530

    fixed build issue

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+/// <reference lib="dom" />
+import * as firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgkWn5izw3H9eMDWTUYQadacWrwsrhQFI",
@@ -9,10 +10,10 @@ const firebaseConfig = {
   appId: "1:733973400570:web:58fd72f030dbca09167243",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.default.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
-const auth = firebase.auth();
+const auth = firebase.default.auth();
 
 export { auth };
 export default db;
